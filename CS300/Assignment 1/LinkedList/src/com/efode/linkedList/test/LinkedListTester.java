939d4a068fb2ff432ffafaf6410b385c54807087
Eric Fode <ericfode@gmail.com>
2012-01-10 23:38:35 -0800

    First commit Worked on Assignment 1

---
+++ CS300/Assignment 1/LinkedList/src/com/efode/linkedList/test/LinkedListTester.java
@@ -0,0 +1,249 @@
+package com.efode.linkedList.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LinkedListTester {
+
+	@Test
+	public void testHashCode() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testAddIntE() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testAddAllIntCollectionOfQextendsE() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGet() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testIndexOf() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testLastIndexOf() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testListIterator() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testListIteratorInt() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testRemoveInt() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testSet() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testSubList() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testAddFirst() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testAddLast() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testDescendingIterator() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testElement() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetFirst() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetLast() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testOffer() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testOfferFirst() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testOfferLast() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testPeek() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testPeekFirst() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testPeekLast() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testPoll() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testPollFirst() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testPollLast() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testPop() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testPush() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testRemove() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testRemoveFirst() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testRemoveFirstOccurrence() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testRemoveLast() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testRemoveLastOccurrence() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testAddE() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testAddAllCollectionOfQextendsE() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testClear() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testContains() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testContainsAll() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testIsEmpty() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testRemoveObject() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testRemoveAll() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testRetainAll() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testSize() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testToArray() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testToArrayEArray() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testIterator() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testEqualsObject() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
---
+++ CS300/Assignment 1/LinkedList/src/com/efode/linkedList/test/NodeTester.java
@@ -0,0 +1,39 @@
+package com.efode.linkedList.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NodeTester {
+
+	@Test
+	public void testNode() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testRemove() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testMove() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testEqualsObject() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetNext() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testAddNext() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
