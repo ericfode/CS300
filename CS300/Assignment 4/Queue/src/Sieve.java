fb94ccf4dd2934065cbf658945ca66adf3e0cc2e
Eric Fode <ericfode@gmail.com>
2012-03-22 18:02:02 -0700

    Reset Commit

---
+++ CS300/Assignment 4/Queue/src/Sieve.java
@@ -0,0 +1,77 @@
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.Iterator;
+public class Sieve {
+	
+	private Queue<Integer> ints;
+	private Queue<Integer> primes;
+	private Boolean calc = false;
+	private int max;
+	
+
+	Sieve()
+	{
+		ints = (Queue<Integer>) new LinkedList<Integer>();
+		primes = (Queue<Integer>) new LinkedList<Integer>();
+	}
+	
+	void computeTo(int n)
+	{
+		max = n;
+		calc = true;
+		ints.clear();
+		primes.clear();
+		
+		for(int i = 2; i < n; i++)
+		{
+			ints.add(i);
+		}
+		Integer p =0;
+		do{
+			p = ints.poll();
+			primes.add(p);
+			
+			for(Iterator<Integer> it = ints.iterator(); it.hasNext();)
+			{
+				Integer curInt = it.next(); 
+				if(curInt%p == 0)
+				{
+					it.remove();
+				}
+			}
+		}while(p < Math.sqrt(n));
+		primes.addAll(ints);
+	}
+	
+	void reportResults()
+	{
+		if(!calc)
+		{
+			throw new IllegalStateException();
+		}
+		for(Integer i : primes)
+		{
+			System.out.print(i.toString() + " " );
+		}
+		System.out.print("\n");
+	}
+	
+	int getMax()
+	{
+		if(!calc)
+		{
+			throw new IllegalStateException();
+		}
+		return max;
+		
+	}
+	
+	int getCount()
+	{
+		if(!calc)
+		{
+			throw new IllegalStateException();
+		}
+		return primes.size();
+	}
+}
